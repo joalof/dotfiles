d4c4b87ac1019f8fef3d146b4e41c90b4f211b69
joalof <lofgren.jocke@gmail.com>
2024-07-18 03:09:44 +0300

    nvim: tabline render() optimization

--- neovim/.config/nvim/lua/utils/tabline.lua
+++ neovim/.config/nvim/lua/utils/tabline.lua
@@ -1,12 +1,13 @@
 local Color = require("lib.color")
+local Path = require('lib.path')
 
 local M = {}
 
--- define colors
+-- define highlights for the tabline
 local norm_fg = Color.from_hl("Normal", "fg")
 local norm_fg_dark = norm_fg:shade(-0.55)
 local norm_bg = Color.from_hl("Normal", "bg")
-local norm_bg_dark = norm_bg:shade(-0.5)
+local norm_bg_dark = norm_bg:shade(-0.35)
 norm_fg = norm_fg:to_css()
 norm_fg_dark = norm_fg_dark:to_css()
 norm_bg = norm_bg:to_css()
@@ -28,45 +29,41 @@ M.hls = {
     },
 }
 
-local function create_pane(f, filename, active)
-    f.add({ "", fg = M.hls.wedge.fg, bg = M.hls.wedge.bg })
-    if filename then
-        f.add({ f.icon(filename) .. " ", fg = f.icon_color(filename), bg = M.hls.text.bg })
-        local text_fg = M.hls.text.fg_inactive
-        if active  then
-            text_fg = M.hls.text.fg_active
+local function create_pane(f, name, active)
+    local hls = M.hls
+    f.add({ "", fg = hls.wedge.fg, bg = hls.wedge.bg })
+    if name then
+        f.add({ f.icon(name) .. " ", fg = f.icon_color(name), bg = hls.text.bg })
+        local text_fg = hls.text.fg_inactive
+        if active then
+            text_fg = hls.text.fg_active
         end
-        f.add({ vim.fn.fnamemodify(filename, ":t"), fg = text_fg, bg = M.hls.text.bg })
+        -- f.add({ vim.fn.fnamemodify(filename, ":t"), fg = text_fg, bg = M.hls.text.bg })
+        f.add({ name, fg = text_fg, bg = hls.text.bg })
     end
-    f.add({ "", fg = M.hls.wedge.fg, bg = M.hls.wedge.bg })
+    f.add({ "", fg = hls.wedge.fg, bg = hls.wedge.bg })
 end
 
-
--- local function get_harpoon_list()
---     local harpoon = require("harpoon")
---     local harpoon_list = harpoon:list()
---     return harpoon_list
--- end
 
 M.display_marks = {}
 
 -- cache marks for display with tabline
+-- this will not be called frequently
 function M.cache_display_marks(list_name)
     local harp_list = require('harpoon'):list(list_name)
+    -- M.display_marks = harp_list.items
     -- only use filenames and not the full path 
-    M.display_marks = harp_list.items
-    -- M.display_marks = {}
-    -- for i, item in ipairs(harp_list.items) do
-    --     M.display_marks[i] = Path(item):name()
-    -- end
+    M.display_marks = {}
+    for i, item in ipairs(harp_list.items) do
+        M.display_marks[i] = Path(item.value):name()
+    end
 end
 
 
--- This will be called a million times so be careful
+-- This will be called a million times so we don't do expensive stuff here
 M.render = function(f)
 
-    -- local current_file = normalize_path(vim.api.nvim_buf_get_name(0))
-    local curr_file = vim.fn.expand('%:p')
+    local curr_file = vim.fn.expand('%:t')
 
     -- local found = false
     -- for _, item in ipairs(items) do
@@ -81,16 +78,11 @@ M.render = function(f)
     --     create_pane(f, current_file, true)
     -- end
 
-    -- add all marks
-    -- for _, item in ipairs(harp_list.items) do
-    --     local active = curr_file == item.value
-    --     create_pane(f, item.value, active)
-    -- end
 
     -- add all marks
     for _, item in ipairs(M.display_marks) do
-        local active = curr_file == item.value
-        create_pane(f, item.value, active)
+        local active = curr_file == item
+        create_pane(f, item, active)
     end
     --
     f.add_spacer()
