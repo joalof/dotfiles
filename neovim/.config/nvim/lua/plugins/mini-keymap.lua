07b06639b600b99d90605f7c4ffc795a4d02945e
joalof <lofgren.jocke@gmail.com>
2025-12-09 22:25:36 +0100

    nvim: jk esc only in i/c mode

--- neovim/.config/nvim/lua/plugins/mini-keymap.lua
+++ neovim/.config/nvim/lua/plugins/mini-keymap.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require('mini.keymap').setup()
         local map_combo = require("mini.keymap").map_combo
-        local mode = { "i", "c", "x", "s" }
+        local mode = {"i", "c"}
         map_combo(mode, "jk", "<BS><BS><Esc>")
         map_combo('t', 'jk', '<BS><BS><C-\\><C-n>')
     end,
