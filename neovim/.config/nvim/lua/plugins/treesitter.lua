aec2bb3160ccac312877f9ef082756712aed7b9d
Joakim Lofgren <joakim@vionlabs.com>
2026-01-02 18:24:37 +0100

    nvim: fixed issues with new treesitter config

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -13,80 +13,65 @@ return {
     opts = {
         highlight = { disable = { csv = true } },
         indent = { disable = { python = true } },
-        ensure_installed = {
-            "bash",
-            "c",
-            "comment",
-            "cpp",
-            "css",
-            "diff",
-            "git_config",
-            "git_rebase",
-            "gitcommit",
-            "gitignore",
-            "html",
-            "javascript",
-            "json",
-            "julia",
-            "lua",
-            "luadoc",
-            "luap",
-            "make",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "query",
-            "regex",
-            "toml",
-            "tsx",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "xml",
-            "yaml",
+        parsers = {
+            bash=true,
+            c=true,
+            comment=true,
+            cpp=true,
+            css=true,
+            diff=true,
+            git_config=true,
+            git_rebase=true,
+            gitcommit=true,
+            gitignore=true,
+            go=true,
+            html=true,
+            javascript=true,
+            json=true,
+            julia=true,
+            lua=true,
+            luadoc=true,
+            luap=true,
+            make=true,
+            markdown=true,
+            markdown_inline=true,
+            python=true,
+            query=true,
+            regex=true,
+            rust=true,
+            toml=true,
+            tsx=true,
+            typescript=true,
+            vim=true,
+            vimdoc=true,
+            xml=true,
+            yaml=true,
+            zig=true,
         },
     },
     config = function(_, opts)
         local ts = require("nvim-treesitter")
-        ts.install(opts.ensure_installed)
 
-        local ignore_filetypes = {
-            checkhealth = true,
-            lazy = true,
-            mason = true,
-            snacks_dashboard = true,
-            snacks_notif = true,
-            snacks_win = true,
-            snacks_input = true,
-            prompt = true,
-        }
+        local ensure_installed = vim.tbl_filter(function(val) return val end, opts.parsers)
+        ts.install(ensure_installed)
 
         -- Enable highlighting/indent on FileType
         vim.api.nvim_create_autocmd("FileType", {
             group = vim.api.nvim_create_augroup("joakim.treesitter_setup", { clear = true }),
             desc = "Enable treesitter highlighting and indentation",
             callback = function(event)
-                if ignore_filetypes[event.match] then
+                local lang = vim.treesitter.language.get_lang(event.match) or event.match
+                if not opts.parsers[lang] then
                     return
                 end
-
-                local lang = vim.treesitter.language.get_lang(event.match)
-                local ft = vim.bo[event.buf].ft
-
-                ts.install({ lang }):await(function(err)
-                    if err then
-                        vim.notify("Treesitter install error for ft: " .. ft .. " err: " .. err)
-                        return
-                    end
-
-                    if not opts.highlight.disable.lang then
-                        pcall(vim.treesitter.start, event.buf)
-                    end
-                    if not opts.indent.disable.lang then
-                        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-                        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-                    end
-                end)
+                
+                if not opts.highlight.disable.lang then
+                    pcall(vim.treesitter.start, event.buf)
+                end
+                if not opts.indent.disable.lang then
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                end
             end,
         })
     end,
