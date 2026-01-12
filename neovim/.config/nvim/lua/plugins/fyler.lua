6257d85a68a89f5ce532d1c5bae691e52e10e1cb
Joakim Lofgren <joakim@vionlabs.com>
2026-01-12 23:30:22 +0100

    nvim: added fyler

---
+++ neovim/.config/nvim/lua/plugins/fyler.lua
@@ -0,0 +1,10 @@
+return {
+    "A7Lavinraj/fyler.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false, -- Necessary for `default_explorer` to work properly
+    opts = {
+        integrations = {
+            icon = "nvim_web_devicons",
+        },
+    },
+}
